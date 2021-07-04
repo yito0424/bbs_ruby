3288f03bbf00e7bec0900e1e56c0328634641135
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-07-05 01:48:19 +0900

    upload local files

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,57 @@
+class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
+  def index
+    @boards = Board.page(params[:page])
+  end
+  def new
+    @board = Board.new(flash[:board])
+  end
+  def create
+    board = Board.new(board_params)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+      redirect_to board
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+      }
+    end
+  end
+
+  def show
+    @comment = Comment.new(board_id: @board.id)
+  end
+
+
+  def edit
+  end
+
+  def update
+    if @board.update(board_params)
+      flash[:notice] = "「#{@board.title}」の掲示板を編集しました"
+      redirect_to @board
+    else
+      redirect_to :back, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
+  end
+
+  def destroy
+    @board.delete
+    redirect_to boards_path, flash: {notice: "「#{@board.title}」の掲示板が削除されました"}
+  end
+  
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'comments/create'
+
+  get 'comments/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
+  # get 'boards/:id', to: 'boards#show'
+  resources :boards
+  resources :comments, only: %i[create destroy]
+end
